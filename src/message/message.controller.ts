93fd37ab36808d911005c8fdfe49b1b4a3b50cbc
Miqueas Gimenez <miquigz02@gmail.com>
2022-12-22 07:09:09 -0300

    prefixglobal apiv1, routes messages

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,26 +1,48 @@
-import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
 import { CreateMessageDto } from './DTO/create-message-dto/create-message-dto';
+import { MessageService } from './message.service';
 
 @Controller('message')
 export class MessageController {
+
+    constructor(private messageService:MessageService){}
+
+
     @Post()
-    create (@Body( ) createMessageDTO: CreateMessageDto){
-        return 'message creado'
+    create (@Body( ) createMessageDTO: CreateMessageDto, @Res() response){
+        this.messageService.createMessage(createMessageDTO)
+        .then( ( message )=>{
+            response.status(HttpStatus.CREATED).json(message);
+        })
+        .catch( (err)=>{
+            response.status(HttpStatus.FORBIDDEN).json({message: "Error in create message", err})
+        })
     }
 
     @Get()
-    getAll(){
-        return 'lists of message'
+    getAll(@Res() response){
+        this.messageService.getAll()
+        .then( (messageList)=>{
+            response.status(HttpStatus.OK).json(messageList);
+        })
+        .catch((err)=> response.status(HttpStatus.FORBIDDEN).json({message: "Error in getAll message", err})
+        )
     }
 
-    @Put()
-    update(@Body() updateMessageDto: CreateMessageDto){
-        return 'update message';
+    @Put(':id')
+    update(@Body() updateMessageDto: CreateMessageDto, @Res() response, @Param('id') idMessage){
+        this.messageService.updateMessage(idMessage, updateMessageDto)
+        .then( (updatedMsg)=>{
+            response.status(HttpStatus.OK).json(updatedMsg)
+        })
+        .catch((err)=> response.status(HttpStatus.FORBIDDEN).json({message: "Error update message", err}))
     }
 
     @Delete(':id')
-    delete(){
-        return 'deleted message'
+    delete( @Param('id') idMessage, @Res() response){
+        this.messageService.deleteMessage(idMessage)
+        .then((res)=> response.status(HttpStatus.OK).json(res))
+        .catch((err)=> response.status(HttpStatus.FORBIDDEN).json({message: `Error delete message, id: ${idMessage} , err: ${err}`}))
     }
 
 }
--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -11,11 +11,11 @@ export class MessageService {
 
     }
 
-    async getAll(){
+    async getAll():Promise<Message[]>{
         return await this.messageRepository.find();
     }
 
-    async createMessage( newMessage: CreateMessageDto ) {
+    async createMessage( newMessage: CreateMessageDto ):Promise<Message> {
         const newMsg = new Message();
         newMsg.message = newMessage.message;
         newMsg.name = newMessage.name;
@@ -23,13 +23,17 @@ export class MessageService {
         return this.messageRepository.save(newMsg);//save in db
     }
 
-    async updateMessage( idMsg:number, updateMessage: CreateMessageDto){
+    async updateMessage( idMsg:number, updateMessage: CreateMessageDto):Promise<Message>{
         //todo evitar params id, reqbody.id
         const msgUpdate = await this.messageRepository.findOneBy({id: idMsg});
         msgUpdate.name = updateMessage.name;
         msgUpdate.message = updateMessage.name
 
         return this.messageRepository.save(msgUpdate);
+    }
+
+    async deleteMessage(idMessage:number):Promise<any>{
+        return await this.messageRepository.delete(idMessage);
     }
 
 }
